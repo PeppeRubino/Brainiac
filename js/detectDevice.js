497c022c8d297e32fcc248c7448f10b20d86bff1
PeppeRubino <121513779+PeppeRubino@users.noreply.github.com>
2023-06-15 01:48:25 +0200

    changes

---
+++ js/detectDevice.js
@@ -0,0 +1,13 @@
+function redirectBasedOnUserAgent() {
+    const isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+  
+    if (isMobile) {
+      window.location.href = "/mobile/index.html";
+    } else {
+      window.location.href = "/index.html";
+    }
+  }
+  
+  redirectBasedOnUserAgent();
+  
+  
