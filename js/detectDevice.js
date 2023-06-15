ab7743b3dd5bc0da7d1c3443d9e8e2bca226db64
PeppeRubino <121513779+PeppeRubino@users.noreply.github.com>
2023-06-15 02:32:02 +0200

    path

--- js/detectDevice.js
+++ js/detectDevice.js
@@ -2,9 +2,9 @@ function redirectBasedOnUserAgent() {
   const isMobile = window.innerWidth <= 768; // Modifica il valore 768 con la larghezza desiderata per i dispositivi mobili
 
   if (isMobile && window.location.pathname !== "/mobile/index.html") {
-    window.location.href = "mobile/index.html";
+    window.location.href = "../mobile/index.html";
   } else if (!isMobile && window.location.pathname !== "/index.html") {
-    window.location.href = "index.html";
+    window.location.href = "../index.html";
   }
 }
 
