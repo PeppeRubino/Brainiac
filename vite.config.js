9e87d597d9459bcb455ad58153c5d23b84b6a2d5
PeppeRubino <121513779+PeppeRubino@users.noreply.github.com>
2023-06-15 05:51:17 +0200

    vite

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,10 @@ module.exports = {
         input: {
           main: 'dist/index.html', // Percorso per la versione nella cartella 'dist'
           mobile: 'mobile/index.html' // Percorso per la versione nella cartella 'mobile'
-        }
+        },
+        external: [
+          // Aggiungi qui eventuali moduli esterni da escludere dal bundle
+        ]
       }
     }
   };
