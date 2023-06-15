d3821f88f67f2fabce2e38fc74d065f007d830d6
PeppeRubino <121513779+PeppeRubino@users.noreply.github.com>
2023-06-15 05:53:17 +0200

    vite

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,8 @@ module.exports = {
     build: {
       rollupOptions: {
         input: {
-          main: 'dist/index.html', // Percorso per la versione nella cartella 'dist'
-          mobile: 'mobile/index.html' // Percorso per la versione nella cartella 'mobile'
+          main: '/dist/index.html', // Percorso per la versione nella cartella 'dist'
+          mobile: '/mobile/index.html' // Percorso per la versione nella cartella 'mobile'
         },
         external: [
           // Aggiungi qui eventuali moduli esterni da escludere dal bundle
