76c4a3e163fa678407bc62467088bf85fad62507
PeppeRubino <121513779+PeppeRubino@users.noreply.github.com>
2023-06-15 14:09:42 +0200

    label

--- mobile/js/index.js
+++ mobile/js/index.js
@@ -309,6 +309,7 @@ const aNumbTitle = document.getElementById('aNumbTitle');
   const modelMeshes = [];
   const originalMaterial = new THREE.MeshStandardMaterial({ color: 0xdecfd7 });
   const assetLoader = new GLTFLoader();
+  assetLoader.responseType = 'arraybuffer';
 
   function loadModel(modelUrl) {
     assetLoader.load(modelUrl.href, function (gltf) {
