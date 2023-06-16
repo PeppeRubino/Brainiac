ba8a56dfff40e968d2f5c3a16231bc0986d44079
PeppeRubino <121513779+PeppeRubino@users.noreply.github.com>
2023-06-16 15:58:16 +0200

    redirect: dist -js function

--- dist/js/redirect.js
+++ dist/js/redirect.js
@@ -2,28 +2,19 @@ function redirectBasedOnUserAgent() {
   const isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
   const isPortrait = window.matchMedia("(orientation: portrait)").matches;
 
-  if ((isMobile && isPortrait && window.location.pathname !== "/mobile/index.html") ||
-      (!isMobile && !isPortrait && window.location.pathname !== "/index.html")) {
-    if (isMobile) {
-      window.location.href = "../mobile/index.html";
-    } else {
-      window.location.href = "../index.html";
-    }
+  if (isMobile && window.location.pathname !== "/mobile/index.html") {
+    window.location.href = "../mobile/index.html";
+  } else if (!isMobile && window.innerWidth < 600 && window.location.pathname !== "/mobile/index.html") {
+    window.location.href = "../mobile/index.html";
+  } else if (!isMobile && window.innerWidth >= 600 && window.location.pathname !== "/dist/index.html") {
+    window.location.href = "../dist/index.html";
   }
 }
 
+function handleOrientationChange() {
+  redirectBasedOnUserAgent();
+}
+
+window.addEventListener("orientationchange", handleOrientationChange);
+window.addEventListener("resize", redirectBasedOnUserAgent);
 redirectBasedOnUserAgent();
-
-
-
-  function handleOrientationChange() {
-    const isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
-    const isPortrait = window.matchMedia("(orientation: portrait)").matches;
-  
-    if (isMobile && !isPortrait && window.location.pathname !== "/mobile/index.html") {
-      window.location.href = "../mobile/index.html";
-    }
-  }
-  
-  window.addEventListener("orientationchange", handleOrientationChange);
-  
